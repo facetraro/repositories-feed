6215fef76d8a2d6f2e2a22791b95bd8c1b31a481
Federico Andrés Cetraro Migliardi <facetraro@gmail.com>
2020-06-27 14:21:49 -0300

    Add more dummy data

--- helpers/dummy_data.js
+++ helpers/dummy_data.js
@@ -4,7 +4,22 @@ const repositoryClass = require('../domain/repository');
 const firstUser = new userClass(1, 'Andres');
 const secondUser = new userClass(2, 'Federico');
 const thirdUser = new userClass(3, 'Marcelo');
-exports.allUsers = [firstUser, secondUser, thirdUser];
+const fourthUser = new userClass(4, 'Olivia');
+const fifthUser = new userClass(5, 'Manuel');
+const sixthUser = new userClass(6, 'Magdalena');
+const seventhUser = new userClass(7, 'Carlos');
+const eighthUser = new userClass(8, 'Sergio');
+
+exports.allUsers = [
+  firstUser,
+  secondUser,
+  thirdUser,
+  fourthUser,
+  fifthUser,
+  sixthUser,
+  seventhUser,
+  eighthUser
+];
 
 const firstRepository = new repositoryClass(
   1,
